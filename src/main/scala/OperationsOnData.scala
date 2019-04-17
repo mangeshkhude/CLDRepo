f4eefb7ce4b5113159125779782eee652fcb6300
mangesh22121992 <mangesh.khude@infobeans.com>
2019-04-17 18:31:03 +0530

    Added code to merge schema

--- src/main/scala/OperationsOnData.scala
+++ src/main/scala/OperationsOnData.scala
@@ -1,6 +1,7 @@
 
+import BroadbandCarrierSelection.parquet_file_location
 import org.apache.spark.SparkContext
-import org.apache.spark.sql.{DataFrame, SQLContext}
+import org.apache.spark.sql.{AnalysisException, DataFrame, SQLContext}
 
 
 object OperationsOnData extends SparkSessionProvider {
@@ -10,26 +11,27 @@ object OperationsOnData extends SparkSessionProvider {
   def main(args: Array[String]): Unit = {
     val sqlContext: SQLContext = new SQLContext(sc)
 
-    // writeParquet(sc, sqlContext)
-    readParquet(sqlContext)
+    //writeParquet(sc, sqlContext)
+    //readParquet(sqlContext)
     convertColumnAsRow(sqlContext)
     seprateNullValues(sqlContext)
+    mergingSchemaDemo(sqlContext)
   }
 
   def writeParquet(sc: SparkContext, sqlContext: SQLContext) = {
     // Read file as RDD
-    val rdd = sqlContext.read.format("csv").option("header", "true").load(".\\testdata\\countries..csv")
+    val rdd = sqlContext.read.format("csv").option("header", "true").load(".\\testdata\\countries.csv")
     // Convert rdd to data frame using toDF; the following import is required to use toDF function.
     val df: DataFrame = rdd.toDF()
     // Write file to parquet
-    df.write.parquet("D:\\testdata\\countries.parquet")
+    df.write.parquet(".\\testdata\\countries.parquet")
   }
 
   def readParquet(sqlContext: SQLContext) = {
     // read back parquet to DF
     val newDataDF = sqlContext.read.parquet(".\\testdata\\countries.parquet")
     // show contents
-    newDataDF.show()
+    newDataDF.show(40)
   }
 
   def convertColumnAsRow(sqlContext: SQLContext): Unit = {
@@ -66,11 +68,20 @@ object OperationsOnData extends SparkSessionProvider {
     val caseClassDS23 = caseClassDS22 :+ Countries(allColumns(23))
     val caseClassDS24 = caseClassDS23 :+ Countries(allColumns(24))
     val caseClassDS25 = caseClassDS24 :+ Countries(allColumns(25))
+    val caseClassDS26 = caseClassDS25 :+ Countries(allColumns(26))
 
     import spark.implicits._
     val newDF = caseClassDS25.toDF()
-    newDF.show()
-
+    try{
+      newDF.write.format("parquet").save("countries.parquet/key=1")
+    } catch {
+      case ex: AnalysisException => {
+        println("File Already Exists")
+      }
+    }
+    val newDataDF1 = sqlContext.read.parquet("countries.parquet/key=1")
+    println("From Parquet file")
+    newDataDF1.show(100)
   }
 
 
@@ -83,6 +94,23 @@ object OperationsOnData extends SparkSessionProvider {
 
     val sqlDataDF = spark.sql("SELECT * FROM countries where Afghanistan = 'Afghanistan'")
     sqlDataDF.show()
+  }
+
+  def mergingSchemaDemo(sqlContext: SQLContext): Unit ={
+    val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+
+    val df = sqlContext.read.parquet(parquet_file_location)
+    df.createOrReplaceTempView("alldata")
+    val sqldf1 = spark.sql("SELECT * FROM alldata")
+    sqldf1.show(1000, false)
+
+    val df2 = sqlContext.read.parquet("countries.parquet/key=1")
+    df2.createOrReplaceTempView("CountryNames")
+    val sqldf2 = spark.sql("SELECT * FROM CountryNames")
+    sqldf2.show()
+
+    val rightjoindf = spark.sql("SELECT alldata.id, alldata.first_name, alldata.last_name, alldata.email, alldata.gender, alldata.title, alldata.country FROM alldata JOIN CountryNames ON alldata.country = CountryNames.Countries AND CountryNames.Countries = 'Argentina'")
+    rightjoindf.show(1000, false)
   }
 
 }
